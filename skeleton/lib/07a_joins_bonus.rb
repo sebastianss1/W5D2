48273c95ea2885d373faa27b3a4a4a5837a998a2
juliajykim <jkim24@gwu.edu>
2021-03-23 17:30:58 -0400

    Started 07 joins exercises

--- skeleton/lib/07a_joins_bonus.rb
+++ skeleton/lib/07a_joins_bonus.rb
@@ -26,6 +26,11 @@ require_relative './sqlzoo.rb'
 def alison_artist
   # Select the name of the artist who recorded the song 'Alison'.
   execute(<<-SQL)
+    SELECT
+      name 
+    FROM
+      albums
+    
   SQL
 end
 
